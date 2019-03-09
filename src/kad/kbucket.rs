46dc0f849e7019fce0b0dba98c0c9a5ad5ea14eb
Justin Evankovich <jaevanko@mtu.edu>
2019-03-09 13:26:03 -0600

    Changed folder layout. Broke kbucket into its own module

--- src/kad/kbucket.rs
+++ src/kad/kbucket.rs
@@ -1,7 +1,3 @@
-extern crate bincode;
-extern crate crossbeam;
-
-use crossbeam::channel;
 use std::collections::VecDeque;
 use std::net::SocketAddr;
 
@@ -151,82 +147,6 @@ impl KBuckets {
         let bucket = (me ^ contact.id).leading_zeros();
         let bucket = self.indices[bucket as usize] as usize;
         self.k_buckets[bucket].contacts.push_back(contact);
-    }
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-enum Payload {
-    Ping,
-    Pong,
-}
-
-impl Payload {
-    fn is_response(&self) -> bool {
-        match self {
-            Payload::Pong => true,
-            _ => false,
-        }
-    }
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Packet {
-    seq_num: u64,
-    payload: Payload,
-}
-
-#[derive(Debug)]
-pub enum Command {
-    Shutdown,
-    Ping(SocketAddr),
-}
-
-pub struct Kad {
-    send: channel::Sender<(Packet, SocketAddr)>,
-
-    known_peers: KBuckets,
-}
-
-impl Kad {
-    pub fn new(send: channel::Sender<(Packet, SocketAddr)>) -> Kad {
-        Kad {
-            send: send,
-            known_peers: KBuckets::new(),
-        }
-    }
-
-    pub fn handle_packet(&mut self, pack: Packet, peer: SocketAddr) {
-        match pack.payload {
-            Payload::Ping => self
-                .send
-                .send((
-                    Packet {
-                        seq_num: pack.seq_num,
-                        payload: Payload::Pong,
-                    },
-                    peer,
-                ))
-                .unwrap(),
-            _ => (),
-        }
-    }
-
-    pub fn handle_command(&mut self, command: Command) -> bool {
-        match command {
-            Command::Shutdown => return true,
-            Command::Ping(peer) => self
-                .send
-                .send((
-                    Packet {
-                        seq_num: 0,
-                        payload: Payload::Ping,
-                    },
-                    peer,
-                ))
-                .unwrap(),
-        };
-
-        false
     }
 }
 
